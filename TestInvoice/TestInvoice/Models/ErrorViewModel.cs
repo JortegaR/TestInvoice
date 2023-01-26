cd9d789abdc91e87f23cdd679da566908bd5de36
JortegaR <ing.ortegajr@gmail.com>
2023-01-25 22:01:06 -0400

    primer commit del template

---
+++ TestInvoice/TestInvoice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestInvoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
